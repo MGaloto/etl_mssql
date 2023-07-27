4d9f391f89d8701c8c449b06eb53488196b243ea
MGaloto <maxigaloto@hotmail.com>
2023-07-27 00:25:10 -0300

    add fix insert + logs

--- configdb.py
+++ configdb.py
@@ -1,7 +1,9 @@
-import pandas as pd
 import pyodbc 
 import os
 from dotenv import load_dotenv
+import json
+from datetime import datetime
+import pprint
 
 load_dotenv()
 
@@ -14,34 +16,66 @@ class ConfigDataBase():
     """
     def __init__(self, table):
         self.table = table
+        self.logsConnection = []
+        self.logsDataBase = []
+        self.date = self.getDateNow()
+
+    def checkLenLogs(self,logs):
+        if len(logs) > 1:
+            return True
+        else:
+            return False
+    
+    def getDateNow(self):
+        fecha_hora_actual = datetime.now()
+        formato = "%Y-%m-%d %H:%M:%S.%f"
+        date = fecha_hora_actual.strftime(formato)[:-3]
+        return date
+        
+    def writeLogs(self, logs, type):
+        new_date = self.date.replace(" ","").replace(":","").replace("-","")
+        with open(f"logs/{new_date + type}.json", 'w') as archivo_json:
+            json.dump(logs, archivo_json)
         
 
     def run(self):
         try:
-            conn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
+            conn = pyodbc.connect(f"Driver={os.environ.get('MSSQL')};"
                           f"Server={os.environ.get('SERVER')};"
                           f"Database={os.environ.get('DB')};"
                           "Trusted_Connection=yes;")
             cursor = conn.cursor()
         except Exception  as e:
             print(f"Error: {e}")
+            self.logsConnection.append({self.date:e})
 
-        total_registros_inical = cursor.execute(f"""SELECT COUNT(*) AS TotalRegistros
-        FROM {self.table};""").fetchall()[0][0]
-        print(f"Total registros inicial: {str(total_registros_inical)}")
+        try:
+            total_registros_inical = cursor.execute(f"""SELECT COUNT(*) AS TotalRegistros
+            FROM {self.table};""").fetchall()[0][0]
+            print(f"Total registros inicial: {str(total_registros_inical)}")
 
-        cursor.execute(f"""WITH CTE AS (
-            SELECT *, ROW_NUMBER() OVER (PARTITION BY ID, MUESTRA, RESULTADO ORDER BY FECHA_COPIA DESC) AS rn FROM {self.table}) 
-        DELETE FROM CTE WHERE rn > 1;""")
+            cursor.execute(f"""WITH CTE AS (
+                SELECT *, ROW_NUMBER() OVER (PARTITION BY ID, MUESTRA, RESULTADO ORDER BY FECHA_COPIA DESC) AS rn FROM {self.table}) 
+            DELETE FROM CTE WHERE rn > 1;""")
 
-        cursor.execute(f"""ALTER TABLE {self.table}
-                ADD CONSTRAINT UQ_UnicoValores UNIQUE ([ID], [MUESTRA], [RESULTADO], [FECHA_COPIA]);""")  
-        total_registros_post = cursor.execute(f"""SELECT COUNT(*) AS TotalRegistros
-        FROM {self.table};""").fetchall()[0][0]
-        print(f"Total registros final: {str(total_registros_post)}")
+            cursor.execute(f"""ALTER TABLE {self.table}
+                    ADD CONSTRAINT UQ_UnicoValores UNIQUE ([ID], [MUESTRA], [RESULTADO], [FECHA_COPIA]);""")  
+            total_registros_post = cursor.execute(f"""SELECT COUNT(*) AS TotalRegistros
+            FROM {self.table};""").fetchall()[0][0]
+            print(f"Total registros final: {str(total_registros_post)}")
+        except Exception  as e:
+            print(f"Error: {e}")
+            self.logsDataBase.append({self.date + str(e)})
 
         cursor.commit()
         cursor.close()
+
+        if self.checkLenLogs(self.logsConnection):
+            self.writeLogs(self.logsConnection, "conn")
+            pprint.pprint(self.logsConnection[0])
+        if self.checkLenLogs(self.logsDataBase):
+            self.writeLogs(self.logsDataBase, "insert")
+            pprint.pprint(self.logsDataBase[0])
 
 
 if __name__ == "__main__":
--- etl.py
+++ etl.py
@@ -3,6 +3,8 @@ import pyodbc
 import os
 from dotenv import load_dotenv
 from datetime import datetime
+import pprint
+import json
 
 load_dotenv()
 
@@ -12,6 +14,9 @@ class ETL():
     """
     def __init__(self, table):
         self.table = table
+        self.logsConnection = []
+        self.logsDataBase = []
+        self.date = self.getDateNow()
         
     def getData(self):
         data = pd.read_csv(
@@ -23,21 +28,33 @@ class ETL():
         formato = "%Y-%m-%d %H:%M:%S.%f"
         date = fecha_hora_actual.strftime(formato)[:-3]
         return date
+    
+    def writeLogs(self, logs, type):
+        new_date = self.date.replace(" ","").replace(":","").replace("-","")
+        with open(f"logs/{new_date + type}.json", 'w') as archivo_json:
+            json.dump(logs, archivo_json)
+    
+    def checkLenLogs(self,logs):
+        if len(logs) > 1:
+            return True
+        else:
+            return False
         
 
     def run(self):
         try:
-            conn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
+            conn = pyodbc.connect(f"Driver={os.environ.get('MSSQL')};"
                           f"Server={os.environ.get('SERVER')};"
                           f"Database={os.environ.get('DB')};"
                           "Trusted_Connection=yes;")
             cursor = conn.cursor()
-        except Exception  as e:
+        except Exception as e:
             print(f"Error: {e}")
+            self.logsConnection.append({self.date:e})
             
         df = self.getData()
-        df["FECHA_COPIA"] = self.getDateNow()
-        
+        df["FECHA_COPIA"] = self.date
+
         total_registros_inical = cursor.execute(f"""SELECT COUNT(*) AS TotalRegistros
         FROM {self.table};""").fetchall()[0][0]
         print(f"Total registros inicial: {str(total_registros_inical)}")
@@ -48,10 +65,14 @@ class ETL():
             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         """
 
-        # Se convierte el DataFrame a una lista de tuplas para usar con executemany
-        valores = df.values.tolist()
-        cursor.executemany(query_insert, valores)
-
+        valores = [tuple(row) for row in df.values]
+        count = 0
+        for valor in valores:
+            count = count + 1
+            try:
+                cursor.executemany(query_insert, [valor])
+            except Exception as e:
+                self.logsDataBase.append({self.date + str(count):str(e)})
 
         total_registros_post = cursor.execute(f"""SELECT COUNT(*) AS TotalRegistros
         FROM {self.table};""").fetchall()[0][0]
@@ -60,13 +81,17 @@ class ETL():
         cursor.commit()
         cursor.close()
 
+        if self.checkLenLogs(self.logsConnection):
+            self.writeLogs(self.logsConnection, "conn")
+            pprint.pprint(self.logsConnection[0])
+        if self.checkLenLogs(self.logsDataBase):
+            self.writeLogs(self.logsDataBase, "insert")
+            pprint.pprint(self.logsDataBase[0])
+
 
 
 if __name__ == "__main__":
     table = "Unificado"
     objeto = ETL(table)
     objeto.run()
-    
-    
-    
     
